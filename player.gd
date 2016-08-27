ee448bfba1d54fe1914376fc083b4708808f404c
Jonas Pfannschmidt <jonas.pfannschmidt@hpe.com>
2016-08-27 12:25:48 +0100

    Improved jumping and added finish line

--- player.gd
+++ player.gd
@@ -4,22 +4,15 @@ const JUMP_FORCE = Vector2(0, -3000)
 const INITIAL_FORCE = Vector2(100, 0)
 const NULL_VECTOR = Vector2(0, 0)
 
-var jumping = true
-
 func _ready():
 	self.add_force(NULL_VECTOR, INITIAL_FORCE)
 	set_fixed_process(true)
 	
-func _integrate_forces(state):
-	pass
-	
 func _fixed_process(delta):
-	if Input.is_action_pressed("jump") and not jumping:
+	var on_ground = get_colliding_bodies().size() > 0
+	
+	if Input.is_action_pressed("jump") and on_ground:
 		self.apply_impulse(NULL_VECTOR, JUMP_FORCE * delta)
-		jumping = true
 
-	if get_colliding_bodies().size() > 0:
-		print("Collision")
-		jumping = false
 	
 
